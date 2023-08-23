22d5b5009dd862315f16065fb2ba9484ea2fb009
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2023-08-23 12:47:55 +0530

    remove commented codes

--- back_end/src/db/dal/resturantDishCategory.ts
+++ back_end/src/db/dal/resturantDishCategory.ts
@@ -1,21 +1,6 @@
 import { DishCategoryCreationInputWithResturantId, DishCategoryOutput, ResturantOutput } from "../../common/interfaces"
 import { DishCategory, Resturant, ResturantDishCategory } from "../models"
 
-// export const createDishCategory = async (payload: DishCategoryInput, t: Transaction): Promise<DishCategoryOutput> => {
-//     const resturantRes = await DishCategory.create(payload, { transaction: t });
-//     return resturantRes
-// }
-
-// export const createResturantDishCategory = async (payload: ResturantDishCategoryInput, t: Transaction): Promise<ResturantDishCategoryOutput> => {
-//     const resturantRes = await ResturantDishCategory.create(payload, { transaction: t });
-//     return resturantRes
-// }
-
-// export const getDishCategoryByName = async (dishCategoryName: string, t: Transaction): Promise<DishCategoryOutput | null> => {
-//     const resturantRes = await DishCategory.findOne({ where: { name: dishCategoryName }, transaction: t });
-//     return resturantRes
-// }
-
 export const createDishCategoryWithResturantId = async (payload: DishCategoryCreationInputWithResturantId): Promise<DishCategoryOutput> => {
     const newDishCategory = await DishCategory.create(payload, { include: [ResturantDishCategory] })
     console.log('New dish category with resturant id : ', newDishCategory)
--- back_end/src/db/services/resturantDishCategory.ts
+++ back_end/src/db/services/resturantDishCategory.ts
@@ -1,46 +1,6 @@
 import { DishCategoryCreationInputWithResturantId, DishCategoryCreationRequestWithResturant, DishCategoryOutput, ResturantOutput } from '../../common/interfaces';
 import * as resturantDishCategoryDal from '../dal/resturantDishCategory';
 
-// export const createResturantDishCategory = async (payload: ResturantDishCategoryCreationInput): Promise<ResturantDishCategoryCreationOutput> => {
-//     const newDishCategoryPayload: DishCategoryInterface = {
-//         id: payload.id,
-//         name: payload.name
-//     }
-
-//     try {
-//         const result: [DishCategoryOutput, ResturantDishCategoryOutput] = await sequelizeConnection.transaction(async (t) => {
-//             let newDishCategory: DishCategoryOutput | null = await resturantDishCategoryDal.getDishCategoryByName(newDishCategoryPayload.name, t)
-
-//             if (!newDishCategory) {
-//                 newDishCategory = await resturantDishCategoryDal.createDishCategory(newDishCategoryPayload, t)
-//             }
-
-//             const newResturantDishCategoryPayload: ResturantDishCategoryInput = {
-//                 id: undefined,
-//                 resturant_id: payload.resturant_id,
-//                 dish_category_id: newDishCategory.id
-//             }
-
-//             const newResturantDishCategory: ResturantDishCategoryOutput = await resturantDishCategoryDal.createResturantDishCategory(newResturantDishCategoryPayload, t)
-
-//             return [newDishCategory, newResturantDishCategory]
-//         })
-
-//         return ({
-//             id: result[0].id,
-//             name: result[0].name,
-//             resturant_id: result[1].resturant_id,
-//             resturant_dish_category_id: result[1].id,
-//         })
-//     } catch (error) {
-//         console.log('Error in createResturantDishCategory service : ', error)
-//         if (error instanceof Error) {
-//             throw new Error(error.message)
-//         }
-//         throw new Error('Unknown Error')
-//     }
-// }
-
 export const createDishCategoryWithResturantId = async (payload: DishCategoryCreationRequestWithResturant): Promise<DishCategoryOutput> => {
     const dalPayload: DishCategoryCreationInputWithResturantId = {
         name: payload.name,
